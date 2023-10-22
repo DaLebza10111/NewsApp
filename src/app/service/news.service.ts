dabbf72c1329676528c10cb709d54a7449d67166
Lebohang Sopazi <72252063+DaLebza10111@users.noreply.github.com>
2023-10-22 04:45:06 +0200

    consume the api endpoints

--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -14,4 +14,16 @@ export class NewsService {
   initSources(){
     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
   }
+
+  getArticleById(source : string){
+
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + 'apiKey=' + this.api_key);
+
+  }
+
+  initArticles(){
+
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+
+  }
 }
